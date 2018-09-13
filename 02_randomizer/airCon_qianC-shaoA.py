f53db7d1aa606eb2e354dc211692227722919120
Cheryl Qian <cqian@stuy.edu>
2018-09-12 20:43:50 -0400

    added randomizer

---
+++ 02_randomizer/airCon_qianC-shaoA.py
@@ -0,0 +1,27 @@
+#Team Aircon - Cheryl Qian, Andrew Shao
+#SoftDev1 pd8
+#K02 -- NO-body expects the Spanish Inquisition!
+#2018-09-12
+
+import random
+
+KREWES = {
+
+        'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],
+
+        'm': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],
+
+        'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']
+
+}
+
+
+krew = input('Choose a krew: (w/m/x)\n') #stores the val of chosen team
+
+if krew in 'wmx' and len(krew) is 1 : #checks if chosen team/key is a single letter in 'wmx'
+    print ('Your krew member is ' + random.choice(KREWES[krew]) + '.' ) #prints random name from chosen key in KREWES
+else :
+    print ('Not a valid krew.')#error message if chosen key is invalid
+   
+
+
